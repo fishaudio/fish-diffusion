cb8984627817eba3c490d8f75d803ac551670bd5
Lengyue <lengyue@lengyue.me>
2023-01-23 16:20:00 -0500

    Chinese Hubert Soft is now general available

--- fish_diffusion/feature_extractors/__init__.py
+++ fish_diffusion/feature_extractors/__init__.py
@@ -1,6 +1,5 @@
 from .builder import FEATURE_EXTRACTORS
-from .chinese_hubert import ChineseHubert
+from .chinese_hubert import ChineseHubertSoft
 from .hubert_soft import HubertSoft
-from .wav2vec2_xlsr import Wav2Vec2XLSR
 
-__all__ = ["ChineseHubert", "Wav2Vec2XLSR", "FEATURE_EXTRACTORS", "HubertSoft"]
+__all__ = ["ChineseHubertSoft", "FEATURE_EXTRACTORS", "HubertSoft"]
--- fish_diffusion/feature_extractors/chinese_hubert.py
+++ fish_diffusion/feature_extractors/chinese_hubert.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 from torch import nn
 from transformers import HubertModel, Wav2Vec2FeatureExtractor
@@ -7,46 +9,8 @@ from .builder import FEATURE_EXTRACTORS
 
 
 @FEATURE_EXTRACTORS.register_module()
-class ChineseHubert(BaseFeatureExtractor):
-    def __init__(self, name="TencentGameMate/chinese-hubert-base", discrete=False):
-        super().__init__()
-
-        self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(name)
-        self.model = HubertModel.from_pretrained(name)
-        self.model.eval()
-
-        self.discrete = discrete
-
-        if discrete:
-            checkpoint = torch.hub.load_state_dict_from_url(
-                "https://github.com/bshall/hubert/releases/download/v0.1/kmeans100-50f36a95.pt",
-                progress=True,
-            )
-            self.cluster_centers = torch.nn.Parameter(checkpoint["cluster_centers_"])
-
-    @torch.no_grad()
-    def forward(self, path_or_audio, sampling_rate=None):
-        audio = self.preprocess(path_or_audio, sampling_rate)
-
-        input_values = self.feature_extractor(
-            audio, sampling_rate=16000, return_tensors="pt"
-        ).input_values
-        input_values = input_values.to(self.model.device)
-
-        features = self.model(input_values).last_hidden_state
-
-        if self.discrete is False:
-            # (B, D, T) -> (B, T, D)
-            return features.transpose(1, 2)
-
-        dist = torch.cdist(features, self.cluster_centers)
-
-        return dist.log_softmax(2).transpose(1, 2)
-
-
-@FEATURE_EXTRACTORS.register_module()
 class ChineseHubertSoft(BaseFeatureExtractor):
-    def __init__(self, ckpt_path=None):
+    def __init__(self, pretrained: bool = True, checkpoint_path: Optional[str] = None):
         super().__init__()
 
         self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(
@@ -55,8 +19,19 @@ class ChineseHubertSoft(BaseFeatureExtractor):
         self.model = HubertModel.from_pretrained("TencentGameMate/chinese-hubert-base")
         self.proj = nn.Sequential(nn.Dropout(0.1), nn.Linear(768, 256))
 
-        if ckpt_path is not None:
-            self.load_state_dict(torch.load(ckpt_path, map_location="cpu"))
+        state_dict = None
+
+        if pretrained is True:
+            state_dict = torch.hub.load_state_dict_from_url(
+                "https://github.com/fishaudio/chinese-hubert-soft/releases/download/v1/chinese-hubert-soft-v1.ckpt",
+                map_location="cpu",
+            )
+
+        if checkpoint_path is not None:
+            state_dict = torch.load(checkpoint_path, map_location="cpu")
+
+        if state_dict is not None:
+            self.load_state_dict(state_dict)
 
     @torch.no_grad()
     def forward(self, path_or_audio, sampling_rate=None):
@@ -70,7 +45,9 @@ class ChineseHubertSoft(BaseFeatureExtractor):
         features = self.model(input_values)
         features = self.proj(features.last_hidden_state)
 
-        # features = features.softmax(2)
-        # features[features < 0.1] = 0
+        # Top-k gating
+        topk, indices = torch.topk(features, 10, dim=2)
+        features = torch.zeros_like(features).scatter(2, indices, topk)
+        features = features / features.sum(2, keepdim=True)
 
         return features.transpose(1, 2)
