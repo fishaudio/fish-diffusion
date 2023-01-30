f7ec804ab7be59e92d2e7ba8fd55fc77eb0f2739
Lengyue <lengyue@lengyue.me>
2023-01-30 15:30:19 -0500

    publish developing diff singer

--- fish_diffusion/feature_extractors/opencpop_transcription.py
+++ fish_diffusion/feature_extractors/opencpop_transcription.py
@@ -38,8 +38,9 @@ class OpenCpopTranscriptionToPhonemesDuration(BaseFeatureExtractor):
 
         cumsum_durations = np.cumsum(durations)
         alignment_factor = mel_len / cumsum_durations[-1]
+        num_classes = len(self.phonemes)
 
-        features = torch.zeros((mel_len, len(self.phonemes) + 1), dtype=torch.float32)
+        features = torch.zeros((mel_len, num_classes + 2), dtype=torch.float32)
 
         for i, (phone, duration, sum_duration) in enumerate(
             zip(phones, durations, cumsum_durations)
@@ -48,10 +49,13 @@ class OpenCpopTranscriptionToPhonemesDuration(BaseFeatureExtractor):
             previous_idx = (
                 int(cumsum_durations[i - 1] * alignment_factor) if i > 0 else 0
             )
-            _temp = torch.zeros(len(self.phonemes) + 1, dtype=torch.float32)
+            _temp = torch.zeros(num_classes + 1, dtype=torch.float32)
             _temp[self.phonemes.index(phone)] = 1
             _temp[-1] = duration
 
-            features[previous_idx:current_idx] = _temp
+            features[previous_idx:current_idx, : num_classes + 1] = _temp
+            features[previous_idx:current_idx, -1] = torch.linspace(
+                0, 1, current_idx - previous_idx
+            )
 
         return features.T
