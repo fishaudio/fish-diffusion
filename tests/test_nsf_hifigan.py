2e52bd36b87d26085ab0e74d878a82de539b65ca
Lengyue <lengyue@lengyue.me>
2023-02-13 13:56:27 -0500

    update vocoder test

--- tests/test_nsf_hifigan.py
+++ tests/test_nsf_hifigan.py
@@ -1,7 +1,7 @@
 import soundfile as sf
 import torchaudio
 
-from fish_diffusion.utils.pitch import get_pitch_parselmouth
+from fish_diffusion.feature_extractors.pitch import ParselMouthPitchExtractor
 from fish_diffusion.vocoders import NsfHifiGAN
 
 source = "dataset/valid-for-opencpop/TruE-干音_0000/0000.wav"
@@ -11,7 +11,10 @@ gan = NsfHifiGAN()
 audio, sr = torchaudio.load(source)
 
 mel = gan.wav2spec(audio)
-f0 = get_pitch_parselmouth(audio, sr, pad_to=mel.shape[-1])
+f0 = ParselMouthPitchExtractor(
+    f0_min=40.0,
+    f0_max=1600.0,
+)(audio, sr, pad_to=mel.shape[-1])
 
 out = gan.spec2wav(mel, f0)
 
