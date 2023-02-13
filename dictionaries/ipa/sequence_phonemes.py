b73a3bb501c4763c40973d793578bfe4b33b64ea
LordElf <36397354+LordElf@users.noreply.github.com>
2023-02-12 21:29:19 -0500

    Update Dockerfile and add IPA support (#14)
    * add_text
    
    * add_ipa
    
    * docker
    
    * ipa
    
    * ipa
    
    * moveIpaToDictionaries

---
+++ dictionaries/ipa/sequence_phonemes.py
@@ -0,0 +1,24 @@
+import unicodedata
+from pathlib import Path
+
+import yaml
+from ipatok import tokenise
+
+# from https://www.internationalphoneticassociation.org/sites/default/files/phonsymbol.pdf
+phonemes_dict = yaml.safe_load(Path(__file__).with_name("symbols.yaml").open())
+# print(phonemes_dict.keys())
+
+# todo: add <bos>, <eos>, <pad>, <unk>, <bar> tokens
+def text_to_sequence(text):
+    seq = []
+    tokens = tokenise(text, tones=True, strict=True)
+    for token in tokens:
+        token = unicodedata.normalize("NFD", token)
+        for i in range(len(token)):
+            char = token[i]
+            char = unicodedata.normalize("NFD", char)
+            if char in phonemes_dict:
+                seq.append(phonemes_dict[char])
+            else:
+                print("Unknown phoneme: {}".format(char))
+    return seq
