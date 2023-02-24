10f865e0b636723b354b6134729c1a763cada2f8
Lengyue <lengyue@lengyue.me>
2023-02-24 02:01:20 -0500

    add pitch adjustable mel spec

---
+++ fish_diffusion/utils/pitch_adjustable_mel.py
@@ -0,0 +1,95 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.utils.data
+from librosa.filters import mel as librosa_mel_fn
+from loguru import logger
+
+
+class PitchAdjustableMelSpectrogram:
+    def __init__(
+        self,
+        sample_rate=44100,
+        n_fft=2048,
+        win_length=2048,
+        hop_length=512,
+        f_min=40,
+        f_max=16000,
+        n_mels=128,
+        center=False,
+    ):
+        self.sample_rate = sample_rate
+        self.n_fft = n_fft
+        self.win_size = win_length
+        self.hop_length = hop_length
+        self.f_min = f_min
+        self.f_max = f_max
+        self.n_mels = n_mels
+        self.center = center
+
+        self.mel_basis = {}
+        self.hann_window = {}
+
+    def __call__(self, y, key_shift=0):
+        factor = 2 ** (key_shift / 12)
+        n_fft_new = int(np.round(self.n_fft * factor))
+        win_size_new = int(np.round(self.win_size * factor))
+
+        if torch.min(y) < -1.0:
+            logger.warning("min value is ", torch.min(y))
+        if torch.max(y) > 1.0:
+            logger.warning("max value is ", torch.max(y))
+
+        mel_basis_key = f"{self.f_max}_{y.device}"
+        if mel_basis_key not in self.mel_basis:
+            mel = librosa_mel_fn(
+                sr=self.sample_rate,
+                n_fft=self.n_fft,
+                n_mels=self.n_mels,
+                fmin=self.f_min,
+                fmax=self.f_max,
+            )
+            self.mel_basis[mel_basis_key] = torch.from_numpy(mel).float().to(y.device)
+
+        hann_window_key = f"{key_shift}_{y.device}"
+        if hann_window_key not in self.hann_window:
+            self.hann_window[hann_window_key] = torch.hann_window(
+                win_size_new, device=y.device
+            )
+
+        y = torch.nn.functional.pad(
+            y.unsqueeze(1),
+            (
+                int((win_size_new - self.hop_length) / 2),
+                int((win_size_new - self.hop_length) / 2),
+            ),
+            mode="reflect",
+        )
+        y = y.squeeze(1)
+
+        spec = torch.stft(
+            y,
+            n_fft_new,
+            hop_length=self.hop_length,
+            win_length=win_size_new,
+            window=self.hann_window[hann_window_key],
+            center=self.center,
+            pad_mode="reflect",
+            normalized=False,
+            onesided=True,
+            return_complex=True,
+        )
+        spec = torch.view_as_real(spec)
+        spec = torch.sqrt(spec.pow(2).sum(-1) + (1e-9))
+
+        if key_shift != 0:
+            size = self.n_fft // 2 + 1
+            resize = spec.size(1)
+            if resize < size:
+                spec = F.pad(spec, (0, 0, 0, size - resize))
+
+            spec = spec[:, :size, :] * self.win_size / win_size_new
+
+        spec = torch.matmul(self.mel_basis[mel_basis_key], spec)
+
+        return spec
