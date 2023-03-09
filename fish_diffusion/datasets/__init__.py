39125501217f0ee721bcf599d3ae8cd9aec7dca6
Lengyue <lengyue@lengyue.me>
2023-03-09 01:11:24 -0500

    remove audio folder and vocoder dataset

--- fish_diffusion/datasets/__init__.py
+++ fish_diffusion/datasets/__init__.py
@@ -1,16 +1,12 @@
-from .audio_folder import AudioFolderDataset
 from .builder import DATASETS
 from .concat import ConcatDataset
 from .naive import NaiveDataset, NaiveSVCDataset
 from .repeat import RepeatDataset
-from .vocoder import VOCODERDataset
 
 __all__ = [
     "DATASETS",
-    "AudioFolderDataset",
     "ConcatDataset",
     "RepeatDataset",
-    "VOCODERDataset",
     "NaiveDataset",
     "NaiveSVCDataset",
 ]
