116edd6eeb2054ec4b9bd712297be3a36bfdf20e
Stardust·减 <52093397+Stardust-minus@users.noreply.github.com>
2023-03-11 20:03:41 +0800

    make lint happy

--- configs/svc_content_vec_multi_node.py
+++ configs/svc_content_vec_multi_node.py
@@ -1,6 +1,10 @@
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-from fish_diffusion.datasets.utils import get_speaker_map_from_subfolder, get_datasets_from_subfolder
+
 from fish_diffusion.datasets.naive import NaiveSVCDataset
+from fish_diffusion.datasets.utils import (
+    get_datasets_from_subfolder,
+    get_speaker_map_from_subfolder,
+)
 from fish_diffusion.utils.pitch import pitch_to_log
 
 _base_ = [
@@ -11,11 +15,17 @@ _base_ = [
 ]
 speaker_mapping = {}
 val_mapping = {}
-speaker_mapping = get_speaker_map_from_subfolder("dataset/train", speaker_mapping) # Update speaker_mapping using subfolders in `dataset/train`.
+speaker_mapping = get_speaker_map_from_subfolder(
+    "dataset/train", speaker_mapping
+)  # Update speaker_mapping using subfolders in `dataset/train`.
 val_mapping = get_speaker_map_from_subfolder("dataset/valid", val_mapping)
 # This will update speaker_mapping to {'speaker0': 0, 'speaker': 1}
-train_datasets = get_datasets_from_subfolder("NaiveSVCDataset", "dataset/train", speaker_mapping)  # Build datasets manually.
-valid_datasets = get_datasets_from_subfolder("NaiveSVCDataset", "dataset/valid", val_mapping)  # Build datasets manually.
+train_datasets = get_datasets_from_subfolder(
+    "NaiveSVCDataset", "dataset/train", speaker_mapping
+)  # Build datasets manually.
+valid_datasets = get_datasets_from_subfolder(
+    "NaiveSVCDataset", "dataset/valid", val_mapping
+)  # Build datasets manually.
 
 dataset = dict(
     train=dict(
@@ -61,25 +71,25 @@ preprocessing = dict(
         type="ParselMouthPitchExtractor",
     ),
 )
-augmentations=[
-        dict(
-            type="FixedPitchShifting",
-            key_shifts=[-5., 5.],
-            probability=0.75,
-        )
+augmentations = [
+    dict(
+        type="FixedPitchShifting",
+        key_shifts=[-5.0, 5.0],
+        probability=0.75,
+    )
 ]
 trainer = dict(
-    devices=8,      # The number of GPUs that Every nodes has
-    num_nodes=6,    # Total nodes
-    max_steps=100000,   # It is highly recommended to make it lower when you use more GPUs
+    devices=8,  # The number of GPUs that Every nodes has
+    num_nodes=6,  # Total nodes
+    max_steps=100000,  # It is highly recommended to make it lower when you use more GPUs
     val_check_interval=None,
     check_val_every_n_epoch=5,  # Steps val is suggested to disable,Because the steps are only calcating in one rank,so Epoch val is better
 )
-#Learning rate setting override
+# Learning rate setting override
 lambda_func = LambdaWarmUpCosineScheduler(
     warm_up_steps=400,
     lr_min=6e-4,  # Too small value is not recommended
     lr_max=2e-3,  # Modify with your total batch_size
-    lr_start=2e-4, 
+    lr_start=2e-4,
     max_decay_steps=40000,
 )
