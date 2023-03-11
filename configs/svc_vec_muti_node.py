a950d086444a8cc0daa853415810ad99fc894cae
Stardust·减 <52093397+Stardust-minus@users.noreply.github.com>
2023-03-11 09:39:14 +0800

    Create svc_vec_muti_node.py

---
+++ configs/svc_vec_muti_node.py
@@ -0,0 +1,71 @@
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from fish_diffusion.datasets.utils import get_speaker_map_from_subfolder, get_datasets_from_subfolder
+from fish_diffusion.datasets.naive import NaiveSVCDataset
+from fish_diffusion.utils.pitch import pitch_to_log
+
+_base_ = [
+    "./_base_/archs/diff_svc_v2.py",
+    "./_base_/trainers/base.py",
+    "./_base_/schedulers/warmup_cosine.py",
+    "./_base_/datasets/naive_svc.py",
+]
+speaker_mapping = {}
+val_mapping = {}
+speaker_mapping = get_speaker_map_from_subfolder("dataset/train", speaker_mapping) # Update speaker_mapping using subfolders in `dataset/train`.
+val_mapping = get_speaker_map_from_subfolder("dataset/valid", val_mapping)
+# This will update speaker_mapping to {'speaker0': 0, 'speaker': 1}
+#print(speaker_mapping)
+train_datasets = get_datasets_from_subfolder("NaiveSVCDataset", "dataset/train", speaker_mapping)  # Build datasets manually.
+valid_datasets = get_datasets_from_subfolder("NaiveSVCDataset", "dataset/valid", val_mapping)  # Build datasets manually.
+
+dataset = dict(
+    train=dict(
+        _delete_=True,  # Delete the default train dataset
+        type="ConcatDataset",  # You want to contact multiple datasets
+        datasets=train_datasets,
+        # Are there any other ways to do this?
+        collate_fn=NaiveSVCDataset.collate_fn,
+    ),
+    valid=dict(
+        _delete_=True,  # Delete the default train dataset
+        type="ConcatDataset",  # You want to contact multiple datasets
+        datasets=valid_datasets,
+        # Are there any other ways to do this?
+        collate_fn=NaiveSVCDataset.collate_fn,
+    ),
+)
+
+model = dict(
+    text_encoder=dict(
+        type="NaiveProjectionEncoder",
+        input_size=256,
+        output_size=256,
+    ),
+    speaker_encoder=dict(
+        input_size=len(speaker_mapping),
+    ),
+    pitch_encoder=dict(
+        preprocessing=pitch_to_log,
+    ),
+    pitch_shift_encoder=dict(
+        type="NaiveProjectionEncoder",
+        input_size=1,
+        output_size=256,
+        use_embedding=False,
+    ),
+)
+preprocessing = dict(
+    text_features_extractor=dict(
+        type="ContentVec",
+    ),
+    pitch_extractor=dict(
+        type="ParselMouthPitchExtractor",
+    ),
+)
+augmentations=[
+        dict(
+            type="FixedPitchShifting",
+            key_shifts=[-5., 5.],
+            probability=0.75,
+        )
+]
