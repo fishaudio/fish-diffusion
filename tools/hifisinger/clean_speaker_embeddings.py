a48cc63c046a5affbfcef491eb91681463674eb8
Lengyue <lengyue@lengyue.me>
2023-03-29 12:45:41 -0400

    code for releasing new pretrained model

--- tools/hifisinger/clean_speaker_embeddings.py
+++ tools/hifisinger/clean_speaker_embeddings.py
@@ -2,12 +2,12 @@ import torch
 
 # No need to keep optimizer states
 data = torch.load(
-    "logs/HiFiSVC/version_None/checkpoints/epoch=122-step=500000-valid_loss=0.86.ckpt",
+    "logs/HiFiSVC/cyrj7ncw/checkpoints/epoch=132-step=540000-valid_loss=0.82.ckpt",
     map_location="cpu",
 )["state_dict"]
 
 # Remove speaker embeddings
 del data["generator.speaker_encoder.embedding.weight"]
 
-torch.save(data, "checkpoints/hifisinger-demo-20230328.ckpt")
+torch.save(data, "checkpoints/hifisinger-pretrained-20230329.ckpt")
 print("Done")
