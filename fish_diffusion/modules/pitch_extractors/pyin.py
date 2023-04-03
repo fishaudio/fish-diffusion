8386c2d164d6cf8896f3413eee29095b6a5bde85
unknown <christopher.yam365@gmail.com>
2023-04-03 01:43:57 -0700

    actually rename this time

---
+++ fish_diffusion/modules/pitch_extractors/pyin.py
@@ -0,0 +1,52 @@
+import librosa
+import numpy as np
+import resampy
+
+from .builder import PITCH_EXTRACTORS, BasePitchExtractor
+
+
+@PITCH_EXTRACTORS.register_module()
+class PyinPitchExtractor(BasePitchExtractor):
+    def __call__(self, x, sampling_rate=44100, pad_to=None):
+        """Extract pitch using libf0 pyin.
+
+        Args:
+            x (torch.Tensor): Audio signal, shape (1, T).
+            sampling_rate (int, optional): Sampling rate. Defaults to 44100.
+            pad_to (int, optional): Pad to length. Defaults to None.
+
+        Returns:
+            torch.Tensor: Pitch, shape (T // hop_length,).
+        """
+
+        assert x.ndim == 2, f"Expected 2D tensor, got {x.ndim}D tensor."
+        assert x.shape[0] == 1, f"Expected 1 channel, got {x.shape[0]} channels."
+
+        if sampling_rate != 22050:
+            y = resampy.resample(x[0].cpu().numpy(), sampling_rate, 22050)
+        else:
+            y = x[0].cpu().numpy()
+
+        # Extract pitch using libf0.pyin
+        pyin_tuple = librosa.pyin(
+            y,
+            frame_length=1024,
+            fmin=self.f0_min,
+            fmax=self.f0_max,
+        )
+
+        frequencies = pyin_tuple[0]
+
+        # Replace NaN frames with zeros
+        nan_indices = np.isnan(frequencies)
+        if np.any(nan_indices):
+            frequencies[nan_indices] = 0
+
+        f0 = frequencies
+
+        # Pad zeros to the end
+        # if pad_to is not None and f0.shape[0] < pad_to:
+        #     total_pad = pad_to - f0.shape[0]
+        #     f0 = np.pad(f0, (total_pad // 2, total_pad - total_pad // 2), "constant")
+
+        return self.post_process(x, sampling_rate, f0, pad_to)
