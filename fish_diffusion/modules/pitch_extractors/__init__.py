74b9da1a32ee382187ab4e762b195def67aa4a3d
Lengyue <lengyue@lengyue.me>
2023-04-03 10:13:08 -0400

    remove libf0

--- fish_diffusion/modules/pitch_extractors/__init__.py
+++ fish_diffusion/modules/pitch_extractors/__init__.py
@@ -1,7 +1,7 @@
 from .builder import PITCH_EXTRACTORS
 from .crepe import CrepePitchExtractor
-from .pyin import PyinPitchExtractor
 from .parsel_mouth import ParselMouthPitchExtractor
+from .pyin import PyinPitchExtractor
 from .world import DioPitchExtractor, HarvestPitchExtractor
 
 __all__ = [
