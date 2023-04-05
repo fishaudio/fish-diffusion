8b5f9a1e4bc0661c7bacd5ecd954f57e2d267720
unknown <christopher.yam365@gmail.com>
2023-04-04 18:10:30 -0700

    rewrite interp in torch

--- fish_diffusion/modules/pitch_extractors/builder.py
+++ fish_diffusion/modules/pitch_extractors/builder.py
@@ -46,9 +46,9 @@ class BasePitchExtractor:
 
         # 去掉0频率, 并线性插值
         nzindex = torch.nonzero(f0).squeeze()
-        f0 = torch.index_select(f0, dim=0, index=nzindex).cpu().numpy()
-        time_org = self.hop_length / sampling_rate * nzindex.cpu().numpy()
-        time_frame = np.arange(pad_to) * self.hop_length / sampling_rate
+        f0 = f0.index_select(dim=0, index=nzindex)
+        time_org = self.hop_length / sampling_rate * nzindex
+        time_frame = torch.arange(pad_to) * self.hop_length / sampling_rate
 
         if f0.shape[0] <= 0:
             return torch.zeros(pad_to, dtype=torch.float, device=x.device)
@@ -56,7 +56,6 @@ class BasePitchExtractor:
         if f0.shape[0] == 1:
             return torch.ones(pad_to, dtype=torch.float, device=x.device) * f0[0]
 
-        # 大概可以用 torch 重写?
-        f0 = np.interp(time_frame, time_org, f0, left=f0[0], right=f0[-1])
+        f0 = torch.interp(time_frame, time_org, f0, left=f0[0], right=f0[-1])
 
-        return torch.from_numpy(f0).to(x.device)
+        return f0.to(x.device)
