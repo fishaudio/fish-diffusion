60317210b621012acd4edfcf3b23e032f09b512e
Lengyue <lengyue@lengyue.me>
2023-05-04 09:40:29 -0400

    temp save config

--- configs/vocoder_nsf_hifigan.py
+++ configs/vocoder_nsf_hifigan.py
@@ -6,16 +6,19 @@ from pytorch_lightning.strategies import DDPStrategy
 from fish_diffusion.utils.pitch import pitch_to_scale
 
 sampling_rate = 44100
+hop_length = 256
 
 trainer = dict(
     accelerator="gpu",
     devices=-1,
     max_epochs=-1,
-    precision=16,
+    precision="32",
+    val_check_interval=5000,
+    check_val_every_n_epoch=None,
     callbacks=[
         ModelCheckpoint(
-            filename="{epoch}-{step}-{valid_loss:.2f}",
-            every_n_epochs=5,
+            filename="{epoch}-{step}-{valid_loss:.4f}",
+            every_n_train_steps=10000,
             save_top_k=-1,
         ),
         LearningRateMonitor(logging_interval="step"),
@@ -25,7 +28,7 @@ trainer = dict(
 
 model = dict(
     type="NSF-HiFiGAN",
-    config="tools/nsf_hifigan/config_v1.json",
+    config="tools/nsf_hifigan/config_v1_256.json",
     # The following code are used for preprocessing
     vocoder=dict(
         type="NsfHifiGAN",
@@ -36,13 +39,21 @@ model = dict(
 dataset = dict(
     train=dict(
         type="NaiveVOCODERDataset",
-        path="/mnt/nvme0/diff-wave-data/train",
-        segment_size=16384,
+        path="/fs/nexus-scratch/lengyue/vocoder-data/train",
+        segment_size=32768,
+        pitch_shift=[-12, 12],
+        loudness_shift=[0.1, 0.9],
+        hop_length=hop_length,
+        sampling_rate=sampling_rate,
     ),
     valid=dict(
         type="NaiveVOCODERDataset",
-        path="/mnt/nvme0/diff-wave-data/valid",
-        segment_size=-1,
+        path="/fs/nexus-scratch/lengyue/vocoder-data/valid",
+        segment_size=None,
+        pitch_shift=None,
+        loudness_shift=None,
+        hop_length=hop_length,
+        sampling_rate=sampling_rate,
     ),
 )
 
@@ -54,18 +65,19 @@ dataloader = dict(
         persistent_workers=True,
     ),
     valid=dict(
-        batch_size=20,
+        batch_size=2,
         shuffle=False,
-        num_workers=4,
+        num_workers=2,
         persistent_workers=True,
     ),
 )
 
 preprocessing = dict(
     pitch_extractor=dict(
-        type="ParselMouthPitchExtractor",
+        type="HarvestPitchExtractor",
         keep_zeros=False,
         f0_min=40.0,
         f0_max=2000.0,
+        hop_length=hop_length,
     ),
 )
