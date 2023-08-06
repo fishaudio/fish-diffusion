ecc63a27dcc79797dd0537226dc9c67b3f66f68c
Lengyue <lengyue@lengyue.me>
2023-08-06 15:34:04 +0000

    Update faiss

--- tools/build_faiss.py
+++ tools/build_faiss.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
 import autofaiss
-import librosa
 import numpy as np
-import torch
 from tqdm import tqdm
 
 source_path = Path("dataset/train")
@@ -19,10 +17,15 @@ for speaker_path in tqdm(list(source_path.iterdir()), desc="Speaker", position=0
         contents.append(data.item()["contents"])
 
     contents = np.concatenate(contents, axis=1).T
+    contents /= np.linalg.norm(contents, axis=1, keepdims=True)
+    print(f"Buiding index for {speaker}..., shape: {contents.shape}")
 
     autofaiss.build_index(
         contents,
         str(source_path / f"{speaker}.index"),
         str(source_path / f"{speaker}.index.json"),
-        metric_type="ip",
+        # HNSW will use more memory than IVF, but it's faster
+        index_key="IVF1024,Flat",
+        # index_key="HNSW64",
+        metric_type="ip",  # inner product, we use cosine similarity
     )
