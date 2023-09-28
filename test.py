a4c49b7ac272c99e63c5a9f7d5ad4181c1466bd3
Lengyue <lengyue@lengyue.me>
2023-09-28 00:31:03 +0000

    remove unused test

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from transformers import AutoTokenizer
-
-t = AutoTokenizer.from_pretrained("bert-base-cased")
-d = np.load(
-    "dataset/LibriTTS/train-clean-100/26/495/26_495_000004_000000.0.data.npy",
-    allow_pickle=True,
-).item()
-print(t.decode(d["contents"][0]))
