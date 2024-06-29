6eb6f897b4d1afb5c14d8d6af4ab37b7194cb143
turning point <40035031+colstone@users.noreply.github.com>
2024-06-29 20:41:41 +0800

    Add multichannel audio convert part in test_nsf_hifigan.py

--- tests/test_nsf_hifigan.py
+++ tests/test_nsf_hifigan.py
@@ -1,5 +1,6 @@
 import soundfile as sf
 import torchaudio
+import torch
 
 from fish_diffusion.modules.pitch_extractors import ParselMouthPitchExtractor
 from fish_diffusion.modules.vocoders import NsfHifiGAN
@@ -9,6 +10,10 @@ source = "raw/2008-1.wav"
 gan = NsfHifiGAN("checkpoints/nsf_hifigan/model", "checkpoints/nsf_hifigan/config.json")
 
 audio, sr = torchaudio.load(source)
+
+#Change the multichannel audio to single channel
+if audio.shape[0] > 1:
+    audio = torch.mean(audio, dim=0, keepdim=True)
 
 mel = gan.wav2spec(audio)
 f0 = ParselMouthPitchExtractor(f0_min=40.0, f0_max=2000.0, keep_zeros=False)(
